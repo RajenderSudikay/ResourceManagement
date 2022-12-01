8edff59bc6f606a73bd250125f386720a2f74619
AMBC_RAJENDER <AMBC_RAJENDER@AMBCHYD0124>
2022-12-01 09:57:14 +0530

    Add project files.

---
+++ ResourceManagement/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ResourceManagement.Models
+{
+    public class EmployeeModel
+    {
+        public string empID { get; set; }
+        public string empName { get; set; }
+        public string empDesignation { get; set; }
+        public string empManager { get; set; }
+        public string empPhone { get; set; }
+        public string empClientName { get; set; }
+        public string empProjectName { get; set; }
+        public string empProjectStatus { get; set; }
+    }
+
+
+    public class TimeSheetModel
+    {
+        public EmployeeModel empData { get; set; }
+        public string empName { get; set; }
+        public string empDesignation { get; set; }
+        public string empManager { get; set; }
+        public string empPhone { get; set; }
+        public string empClientName { get; set; }
+        public string empProjectName { get; set; }
+        public string empProjectStatus { get; set; }
+    }
+}
