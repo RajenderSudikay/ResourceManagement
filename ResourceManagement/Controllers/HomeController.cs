b453273696d258e1c418005c44d811eddcc01e98
rajendersudikay <rajender.sudikay@gmail.com>
2022-12-02 20:19:52 +0530

    - Code refactor

--- ResourceManagement/Controllers/HomeController.cs
+++ ResourceManagement/Controllers/HomeController.cs
@@ -1,20 +1,12 @@
-﻿using Dapper;
-using ResourceManagement.Entity_Framework;
-using ResourceManagement.Models;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace ResourceManagement.Controllers
 {
     using Models;
-
     public class HomeController : Controller
     {
-
         public ActionResult Login()
         {
             return View();
@@ -51,10 +43,7 @@ namespace ResourceManagement.Controllers
                 }
             }
             return View(loginModel);
-
         }
-
-
 
         public ActionResult Dashboard()
         {
@@ -73,7 +62,6 @@ namespace ResourceManagement.Controllers
                 }
             }
             return RedirectToAction("Login");
-
         }
 
         public ActionResult TimeSheet()
@@ -92,18 +80,11 @@ namespace ResourceManagement.Controllers
                     return RedirectToAction("Login");
                 }
             }
-
-
             return RedirectToAction("Login");
         }
 
         public ActionResult AddTimeSheet(List<RMA_TimeSheetAjaxModel> timesheetmodel)
         {
-            //Employye
-            //Database
-            //Employee 
-            //
-            ViewBag.Message = "Your contact page.";
             UpdateTimeSheetStatus(timesheetmodel);
             return View("~/Views/Home/Contact.cshtml");
         }
@@ -111,38 +92,12 @@ namespace ResourceManagement.Controllers
 
         public void UpdateTimeSheetStatus(List<RMA_TimeSheetAjaxModel> timesheetmodel)
         {
-            //SqlDataReader rdr = null;
-            //SqlConnection con = null;
-            //SqlCommand cmd = null;
-
-            //con = new SqlConnection(ConfigurationManager.ConnectionStrings["TimesheetDBEntities"].ToString());
-            //String FirstName;
-            //String LastName;
-
-            //con.Open();
-            //string CommandText = "SELECT employee_id, employee_name FROM [emp_info] WHERE employee_id='1001'";
-            //cmd = new SqlCommand(CommandText);
-            //cmd.Connection = con;
-            //rdr = cmd.ExecuteReader();
-
-            //while (rdr.Read())
-            //{
-            //    FirstName = rdr["employee_id"].ToString();
-            //    LastName = rdr["employee_name"].ToString();
-            //}
-
             var employeeModel = Session["UserModel"] as RMA_EmployeeModel;
-
             using (var context = new TimeSheetEntities())
             {
-                // Create and save a new Students
-                Console.WriteLine("Adding new students");
-
                 var tasks = new List<ambctaskcapture>();
-
                 foreach (var model in timesheetmodel)
                 {
-                    //var customers = context.Set<TimeSheet>();
                     var timesheet = new ambctaskcapture()
                     {
                         taskdate = System.Convert.ToDateTime(model.Date),
@@ -166,32 +121,7 @@ namespace ResourceManagement.Controllers
                         uniquekey = model.Date + "_" + model.IncidentNumber + "_" + model.TimeSpent,
                         weekno = 29
                     };
-
                     tasks.Add(timesheet);
-
-                    //var abc = new List<string>();
-
-                    //abc.Add("employee_id");
-                    //abc.Add("employee_name");
-                    //abc.Add("employee_desg");
-
-                    //var student = (from s in context.emp_info
-                    //               where s.employee_id == "1001"
-                    //               select s).FirstOrDefault<EmployeeModel>();
-
-
-                    ////Get student name of string type
-                    //var studentName = context.Database.SqlQuery<string>("Select employee_name from [emp_info] where employee_id='1001'")
-                    //                        .FirstOrDefault();
-
-
-                    //var studentList = context.Database.SqlQuery<string>("Select * from Students").ToList();
-
-
-                    //context.Database.ExecuteSqlCommand("insert into ambctaskcapture(taskdate,category) values('" + timesheet.taskdate + "', '" + timesheet.category + "')");
-
-
-
                 }
 
                 context.ambctaskcaptures.AddRange(tasks);
--- ResourceManagement/Models/EmployeeModel.cs
+++ ResourceManagement/Models/EmployeeModel.cs
@@ -1,47 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace ResourceManagement.Models
+﻿namespace ResourceManagement.Models
 {
-    public class RMA_TimeSheet
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int tslogno { get; set; }
-        public string employeeid { get; set; }
-        public string employeename { get; set; }
-        public string empdesignation { get; set; }
-        public string empmanager { get; set; }
-        public string empcontactnumber { get; set; }
-        public string clientname { get; set; }
-        public string projectname { get; set; }
-        public string projstatus { get; set; }
-        public string taskdate { get; set; }
-        public string category { get; set; }
-        public string incidentnumber { get; set; }
-        public string taskdetails { get; set; }
-        public string requester { get; set; }
-        public string callpriority { get; set; }
-        public string callstatus { get; set; }
-        public string closeddate { get; set; }
-        public string timespent { get; set; }
-        public string comments { get; set; }
-    }
-
     public class RMA_EmployeeModel
     {
         public emp_info empInfo { get; set; }
         public emp_project projectInfo { get; set; }
-
-        //public string Employee_Id { get; set; }
-        //public string Employee_Name { get; set; }
-        //public string Employee_Desg { get; set; }
-        //public string Employee_Mailid { get; set; }
-        //public string Employee_Mobile { get; set; }
-        //public string Employee_Report_Manager { get; set; }
-        //public string Employee_Client_Name { get; set; }
-        //public string Employee_Project_Name { get; set; }
-        //public string Employee_Project_Status { get; set; }
     }
 
 
