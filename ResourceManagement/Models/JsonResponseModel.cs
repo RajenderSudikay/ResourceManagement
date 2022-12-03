988ea70dbf3c4a9fb048afe069973de2e80876ee
rajendersudikay <rajender.sudikay@gmail.com>
2022-12-03 12:20:46 +0530

    - added validation to the fome - Error message dispaly on submitting the time sheet reports

---
+++ ResourceManagement/Models/JsonResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Web;
+
+namespace ResourceManagement.Models
+{
+    public class JsonResponseModel
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+
+        [JsonPropertyName("statuscode")]
+        public int StatusCode { get; set; } = 500;
+
+    }
+}
