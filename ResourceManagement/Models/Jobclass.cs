26ea48dde847ce188a38b63caebb25d2dee1b46b
Rajender Sudikay <rajender.sudikay@gmail.com>
2023-05-24 08:40:04 +0530

    - Removed unused code

--- ResourceManagement/Models/Jobclass.cs
+++ ResourceManagement/Models/Jobclass.cs
@@ -16,27 +16,6 @@ namespace ResourceManagement.Models
                     var contents = client.DownloadString(jobURL);                 
                 }
             }
-
-            //using (MailMessage mm = new MailMessage(ConfigurationManager.AppSettings["SMTPUserName"], "rajendersudikay@ambconline.com"))
-            //{
-            //    mm.Subject = "Schedular Email" + DateTime.Now.ToString();
-            //    var emailBody = "Hello Schedular Job " + DateTime.Now.ToString();
-            //    mm.Body = emailBody;
-
-            //    mm.IsBodyHtml = true;
-            //    SmtpClient smtp = new SmtpClient();
-            //    smtp.Host = ConfigurationManager.AppSettings["SMTPHost"];
-            //    smtp.EnableSsl = true;
-            //    NetworkCredential credentials = new NetworkCredential();
-            //    credentials.UserName = ConfigurationManager.AppSettings["SMTPUserName"];
-            //    credentials.Password = ConfigurationManager.AppSettings["SMTPPassword"];
-            //    smtp.UseDefaultCredentials = true;
-            //    smtp.Credentials = credentials;
-            //    smtp.Port = System.Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
-            //    smtp.Send(mm);
-            //}
         }
-
     }
-
 }
