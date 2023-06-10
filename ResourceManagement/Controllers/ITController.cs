ceb0c1da981da8f4de8a3e92e2f9f51c6f66392b
Rajender Sudikay <rajender.sudikay@gmail.com>
2023-06-10 08:31:20 +0530

    - IT menu functionality updates

--- ResourceManagement/Controllers/ITController.cs
+++ ResourceManagement/Controllers/ITController.cs
@@ -1,6 +1,10 @@
-﻿using ResourceManagement.Models;
+﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using ResourceManagement.Models;
+using ResourceManagement.Models.IT;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,5 +37,32 @@ namespace ResourceManagement.Controllers
             var employeeModel = Session["UserModel"] as RMA_EmployeeModel;
             return View(employeeModel);
         }
+
+        public JsonResult ITReportsUploadAjax(ITUpload fileData)
+        {
+            var model = new RMA_StatusReportModel();
+            var employeeModel = Session["UserModel"] as RMA_EmployeeModel;
+            model.RMA_EmployeeModel = employeeModel;
+
+            HttpPostedFileBase file = fileData.ExcelFile;
+
+            if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+            {
+                string dir = @"C:\\inetpub\\wwwroot\\UploadedFiles\2023\March";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                string _FileName = Path.GetFileName(file.FileName);
+                string _path = Path.Combine(dir, _FileName);
+                file.SaveAs(_path);
+            }
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+
+        
     }
 }
---
+++ ResourceManagement/Models/IT/ITUpload.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResourceManagement.Models.IT
+{
+    public class ITUpload
+    {
+
+        //To change label title value  
+        [DisplayName("Employee Name")]
+        public string EmployeeName { get; set; }
+
+        [DisplayName("Employee ID")]
+        public string EmployeeID { get; set; }
+
+        //To change label title value  
+        [DisplayName("Month")]
+        public string Month { get; set; }
+        public string TemplateType { get; set; }
+        public string ProjectID { get; set; }
+        public string ClientName { get; set; }
+
+        //To change label title value  
+        [DisplayName("Upload Report File")]
+        public string ExcelPath { get; set; }
+        public HttpPostedFileBase ExcelFile { get; set; }
+        public List<SelectListItem> EmployeeList { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> MonthList { get; set; } = new List<SelectListItem>();
+        public string FieldsIndexJson { get; set; }
+        public string ValuesMappingJson { get; set; }
+        public string SuccessMessage { get; set; }
+        public string FailureMessage { get; set; }
+        public string SelectedColumnIndex { get; set; }
+        public string SelectedColumnName { get; set; }
+        public string ToolName { get; set; }
+        public string Uploadedby { get; set; }
+        public bool IsAuditReport { get; set; }
+        public string ProjectCategory { get; set; }
+    }
+}
