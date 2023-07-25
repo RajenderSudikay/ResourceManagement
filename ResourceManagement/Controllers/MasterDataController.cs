ffbf85592895a58ebf4866dd62efc6d06af1c531
Rajender Sudikay <rajender.sudikay@gmail.com>
2023-07-25 10:30:39 +0530

    - Master data basic flow implemented

--- ResourceManagement/Controllers/MasterDataController.cs
+++ ResourceManagement/Controllers/MasterDataController.cs
@@ -1,17 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ResourceManagement.Controllers
 {
+    using Newtonsoft.Json;
+    using ResourceManagement.Models;
+    using ResourceManagement.Models.MasterData;
+    using System.Linq;
+
     public class MasterDataController : Controller
     {
         // GET: MasterData
-        public ActionResult Index()
+        public ActionResult AddUpdate()
         {
-            return null;
+            var model = FillDefaultMasterModel();
+            return View(model);
         }
+
+        private MasterModel FillDefaultMasterModel()
+        {
+            var employeeModel = Session["UserModel"] as RMA_EmployeeModel;
+            var MasterModel = new MasterModel();
+            MasterModel.RMA_EmployeeModel = employeeModel;
+            return MasterModel;
+        }
+
+        public JsonResult ViewMasterData(InputModel inputModel)
+        {           
+            using (TimeSheetEntities db = new TimeSheetEntities())
+            {
+                var details = new object();
+
+                if(inputModel.TypeOfData == "Category")
+                {
+                    details = db.Categories;
+                }
+
+                var jsonReponse = JsonConvert.SerializeObject(details);
+                return Json(jsonReponse, JsonRequestBehavior.AllowGet);
+            }
+        }
+
     }
 }
---
+++ ResourceManagement/Models/MasterData/MasterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ResourceManagement.Models.MasterData
+{
+    public class MasterModel
+    {
+        public RMA_EmployeeModel RMA_EmployeeModel { get; set; } = new RMA_EmployeeModel();
+
+        public InputModel InputModel { get; set; } = new InputModel();
+    }
+
+    public class InputModel
+    {
+        public string TypeOfData { get; set; }       
+    }
+}
